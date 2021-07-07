01bc5b4659fdf21431793f75bac421bd166a5517
Hoang Luu <Hoang.Luu@aa.com>
2021-07-07 10:24:53 -0500

    A and B Exp completed

--- exp_a/exp_a_dev.js
+++ exp_a/exp_a_dev.js
@@ -1,152 +1,75 @@
-if (typeof TlIqCustm !== 'undefined') {
-	//add relevant files to git
-	
-	//create the child object if does not exist
-	if(typeof TlIqCustm.ChangeFlightsPhaseTwo === 'undefined'){
+if ('undefined' != typeof TlIqCustm && void 0 === TlIqCustm.languageSelectorFooterPhase2)
+	try {
+		(TlIqCustm.languageSelectorFooterPhase2 = {
+			d_cookie: 'AD_OPT0334_HomeRedesignCountryLanguage_Control_June2021',
+			e_cookie: 'AE_OPT0334_HomeRedesignCountryLanguage_Control_June2021',
+			o_cookie: 'AO_OPT0334_HomeRedesignCountryLanguage_Control_June2021',
+			cookie: 'A_OPT0334_HomeRedesignCountryLanguage_Control_June2021',
+			setCookie: function (e) {
+				TlIqCustm.ckz.cookieValueExists('tgck', e, '|', !1) ||
+					TlIqCustm.ckz.cookieAppend('tgck', e, '|');
+			},
+			dom_check: [],
+			loop: 0,
+			loop_max: 10,
+			initDomCheck: function () {
+				this.dom_check.push(jQuery('body'));
+			},
+			isTablet: function () {
+				return 'undefined' != typeof $device && !0 === $device.tablet;
+			},
+			isDesktop: function () {
+				return 'undefined' != typeof $device && !0 === $device.desktop;
+			},
 
-		try {
-
-			TlIqCustm.ChangeFlightsPhaseTwo = {
-	
-	
-				d_cookie: 'AD_OPT0337_ChangeResUITweak_ChangeTripAndText_June2021',
-				e_cookie: 'AE_OPT0337_ChangeResUITweak_ChangeTripAndText_June2021',
-				o_cookie: 'AO_OPT0337_ChangeResUITweak_ChangeTripAndText_June2021',
-	
-				cookie: 'A_OPT0337_ChangeResUITweak_ChangeTripAndText_June2021',
-	
-				setCookie: function (cookie) {
-					if (!TlIqCustm.ckz.cookieValueExists('tgck', cookie, '|', false)) {
-						TlIqCustm.ckz.cookieAppend('tgck', cookie, '|');
-					}
-				},
-	
-	
-				//the dom check array
-				dom_check: [],
-	
-				//push objects to the dom check array
-				initDomCheck: function () {
-					this.dom_check.push(jQuery('body'));
-					this.dom_check.push(jQuery('#aa-footer'));
-				},
-	
-				//build content if needed
-				ataPrep: function () {
-                    
-				},
-
-
-                aa: function(){
-
-                    var dropdown = ''
-                    // dropdown += ''
-                    // dropdown += '<form name="form1" id="form1" action="/action_page.php">'
-                    // dropdown += '<select name="subject" id="subject">'
-                    // dropdown += '<option value="" selected="selected">'
-                    // dropdown += '<img alt="United States" src="/content/images/chrome/rebrand/aa-icons-flags-sprite.png">English'
-                    // dropdown += '</option>'
-                    // dropdown += '<option value="">Spanish</option>'
-                    // dropdown += '<option value="">French</option>'
-                    // dropdown += '</select>'
-                    // dropdown += '</form>'
-
-
-                    
-                    jQuery('#aa-footer').append(dropdown)
-                    
-                },
-
-
-				//main 
-				ataMain: function () {
+			ataMain: function () {
+				if (
+					TlIqCustm.languageSelectorFooterPhase2.isDesktop() ||
+					TlIqCustm.languageSelectorFooterPhase2.isTablet()
+				)
 					try {
-	
-						this.initDomCheck();
-						if (TlIqCustm.domCheck(this.dom_check)) {
-							//do stuff
-
-                            
-
-							this.setCookie(this.cookie);
-						}
-						else {
-							TlIqCustm.ChangeFlightsPhaseTwo.setCookie(TlIqCustm.ChangeFlightsPhaseTwo.d_cookie);
-						}
-	
-	
+						this.initDomCheck(),
+							TlIqCustm.domCheck(this.dom_check)
+								? TlIqCustm.languageSelectorFooterPhase2.setCookie(
+										TlIqCustm.languageSelectorFooterPhase2.cookie
+								  )
+								: TlIqCustm.languageSelectorFooterPhase2.setCookie(
+										TlIqCustm.languageSelectorFooterPhase2.d_cookie
+								  );
+					} catch (e) {
+						TlIqCustm.languageSelectorFooterPhase2.setCookie(
+							TlIqCustm.languageSelectorFooterPhase2.e_cookie
+						);
 					}
-	
-					catch (e) {
-						TlIqCustm.ChangeFlightsPhaseTwo.setCookie(TlIqCustm.ChangeFlightsPhaseTwo.e_cookie);
-	
-						console.log('ata_failure');
-	
-						console.log("error object:");
-						console.log(e);
-						console.log();
-	
-						console.log("error object toString():");
-						console.log("\t" + e.toString());
-	
-						console.log();
-						console.log("error object attributes: ");
-						console.log('\tname: ' + e.name + ' message: ' + e.message + ' at: ' + e.at + ' text: ' + e.text);
-	
-						console.log();
-						console.log("error object stack: ");
-						console.log(e.stack);
-	
-					}
-				}
-			};
-	
-			TlIqCustm.ChangeFlightsPhaseTwo.ataPrep();
-			
+			},
+		}),
 			TlIqCustm.poll(
 				function () {
-					//return the page load confirmation conditional
-					return jQuery('#aa-footer').length > 0;
+					return true;
 				},
 				function () {
-					//execute main function upon success
-					TlIqCustm.ChangeFlightsPhaseTwo.ataMain();
+					TlIqCustm.languageSelectorFooterPhase2.ataMain();
 				},
 				function () {
-					//otherwise log OOO cookie on failure
-					TlIqCustm.ChangeFlightsPhaseTwo.setCookie(TlIqCustm.ChangeFlightsPhaseTwo.o_cookie);
+					(TlIqCustm.languageSelectorFooterPhase2.isDesktop() ||
+						TlIqCustm.languageSelectorFooterPhase2.isTablet()) &&
+						TlIqCustm.languageSelectorFooterPhase2.setCookie(
+							TlIqCustm.languageSelectorFooterPhase2.o_cookie
+						);
 				}
 			);
-	
-	
-		}
-	
-		catch (e) {
-			//use the hard coded error cookie value here, in place of 'error_cookie'
-			if (!TlIqCustm.ckz.cookieValueExists('tgck', 'AE_OPT0337_ChangeResUITweak_ChangeTripAndText_June2021', '|', false)) {
-				TlIqCustm.ckz.cookieAppend('tgck', 'AE_OPT0337_ChangeResUITweak_ChangeTripAndText_June2021', '|');
-			}
-	
-			console.log('ata_failure');
-	
-			console.log("error object:");
-			console.log(e);
-			console.log();
-	
-			console.log("error object toString():");
-			console.log("\t" + e.toString());
-	
-			console.log();
-			console.log("error object attributes: ");
-			console.log('\tname: ' + e.name + ' message: ' + e.message + ' at: ' + e.at + ' text: ' + e.text);
-	
-			console.log();
-			console.log("error object stack: ");
-			console.log(e.stack);
-	
-		}
-
+	} catch (e) {
+		(TlIqCustm.languageSelectorFooterPhase2.isDesktop() ||
+			TlIqCustm.languageSelectorFooterPhase2.isTablet()) &&
+			(TlIqCustm.ckz.cookieValueExists(
+				'tgck',
+				'AE_OPT0334_HomeRedesignCountryLanguage_Control_June2021',
+				'|',
+				!1
+			) ||
+				TlIqCustm.ckz.cookieAppend(
+					'tgck',
+					'AE_OPT0334_HomeRedesignCountryLanguage_Control_June2021',
+					'|'
+				));
 	}
-	
-}
-
--- exp_b/exp_b_dev.js
+++ exp_b/exp_b_dev.js
@@ -1,10 +1,10 @@
-if ('undefined' != typeof TlIqCustm && void 0 === TlIqCustm.countryLanguageSelector)
+if ('undefined' != typeof TlIqCustm && void 0 === TlIqCustm.languageSelectorFooterPhase2)
 	try {
-		(TlIqCustm.countryLanguageSelector = {
-			d_cookie: 'BD_OPT0310_HomeRedesignCountryLanguage_Test_Mar2021',
-			e_cookie: 'BE_OPT0310_HomeRedesignCountryLanguage_Test_Mar2021',
-			o_cookie: 'BO_OPT0310_HomeRedesignCountryLanguage_Test_Mar2021',
-			cookie: 'B_OPT0310_HomeRedesignCountryLanguage_Test_Mar2021',
+		(TlIqCustm.languageSelectorFooterPhase2 = {
+			d_cookie: 'BD_OPT0334_HomeRedesignCountryLanguage_Test_June2021',
+			e_cookie: 'BE_OPT0334_HomeRedesignCountryLanguage_Test_June2021',
+			o_cookie: 'BO_OPT0334_HomeRedesignCountryLanguage_Test_June2021',
+			cookie: 'B_OPT0334_HomeRedesignCountryLanguage_Test_June2021',
 			setCookie: function (e) {
 				TlIqCustm.ckz.cookieValueExists('tgck', e, '|', !1) ||
 					TlIqCustm.ckz.cookieAppend('tgck', e, '|');
@@ -25,56 +25,9 @@ if ('undefined' != typeof TlIqCustm && void 0 === TlIqCustm.countryLanguageSelec
 			},
 
 			moveSection: function () {
-				// jQuery('#aa-footer > div.container > div.social').attr('Style', 'margin-top:20px'),
-				// 	jQuery('#aa-footer > div.container > div.left').attr('Style', 'margin-top:20px'),
-
-				// <div id="language-selector" data-behavior="dropdown-wrapper" class="js-dropdown-wrapper">
-				//     <a href="#" data-behavior="dropdown-trigger" class="js-dropdown-trigger">
-				//         <span class="aa-language-select-indicator">
-				//             <img alt="United States" src="/content/images/chrome/rebrand/aa-icons-flags-sprite.png">
-				//         </span>
-				//         English
-				//         <img alt="Click to change language and/or country / region" src="/content/images/chrome/rebrand/down-arrow.png" class="dropdown-indicator">
-				//     </a>
-				// </div>
-				// <div data-behavior="dropdown-panel" style="display: none;" class="js-dropdown-panel">
-
-				//     jQuery('#splashForm')
-				// </div>
-
-				// <script>
-				//     function deleteBackCall() {
-				//         window.history.pushState("", "", '/homePage.do');
-				//     }
-				// </script>
-
-				//     </li>
-				var temp =
-					'<li id="countrySelector" class="menu-item-utility is-hidden-mobile   js-dropdown" data-behavior="dropdown">';
-				temp +=
-					'<div id="    " data-behavior="dropdown-wrapper" class="js-dropdown-wrapper" style="border: 1px solid rgb(0, 70, 127); border-radius: 3px; width: 195px; padding: 0px;">';
-				temp +=
-					'<a href="#" data-behavior="dropdown-trigger" class="js-dropdown-trigger" style="text-align: left; color: rgb(0, 70, 127); font-size: 12px; font-weight: bold; padding: 10px 15px; background-color: transparent; width: 195px;">';
-				temp +=
-					'<span class="aa-language-select-indicator" style="width: calc(22px); height: calc(18px); border-radius: 3px; margin-right: 10px;">';
-				temp +=
-					'<img alt="United States" src="/content/images/chrome/rebrand/aa-icons-flags-sprite.png" style="margin-top: -3.5px; margin-left: -151px;">';
-				temp += '</span>';
-				temp += 'English';
-				temp +=
-					'<img alt="Click to change language and/or country / region" src="/content/images/chrome/rebrand/down-arrow.png" class="dropdown-indicator" style="margin: 7px 3px; padding: 0px; float: right; width: 10px;">';
-				temp += '</a>';
-				temp += '</div>';
-				temp +=
-					'<div data-behavior="dropdown-panel" style="display: none;" class="js-dropdown-panel">';
-
-				temp += '</div>';
-				jQuery('.js-dropdown-panel').append(jQuery('#splashForm'));
-				temp += '</li>';
-
 				var countrySelector = jQuery('#countrySelector');
 				jQuery('.js-dropdown-wrapper').css({
-					border: '1px solid rgb(0, 70, 127)',
+					border: '1px solid #4A4A4A',
 					'border-radius': '3px',
 					width: '240px',
 					height: '50px',
@@ -82,19 +35,25 @@ if ('undefined' != typeof TlIqCustm && void 0 === TlIqCustm.countryLanguageSelec
 				});
 				jQuery('.js-dropdown-trigger').css({
 					'text-align': 'left',
-					color: 'rgb(0, 70, 127)',
+					color: '#4A4A4A',
 					'font-size': '12px',
 					'font-weight': 'bold',
-					padding: '10px 15px',
-					'background-color': 'transparent',
-					width: '195px',
+					padding: '12px 15px',
+					'background-color': '#D0DAE0',
+					width: '240px',
+				});
+				jQuery('.aa-language-select-indicator').find('img').css({ 'mix-blend-mode': 'multiply' });
+				jQuery('.js-dropdown-panel').css({
+					left: '0px',
+					'min-width': '240px',
 				});
 
-				jQuery('#aa-footer').prepend('<hr style="margin-top:40px;">');
-				jQuery('#aa-footer').prepend('<hr style="margin-top:40px;">');
-				jQuery('#aa-footer').prepend(jQuery('js-dropdown-panel'));
-				jQuery('#aa-footer').prepend(temp), jQuery('.js-dropdown-panel').remove();
-				jQuery('#aa-footer').prepend(countrySelector),
+				var div =
+					'<div id="languageSelector-footer" class="container" style="margin-bottom: 21px" />';
+				jQuery('#aa-footer').prepend(div);
+
+				jQuery('#languageSelector-footer').prepend(countrySelector),
+					jQuery('.dropdown-indicator').css({ 'margin-left': '120px' }),
 					setTimeout(function () {
 						TlIqCustm.poll(
 							function () {
@@ -106,9 +65,9 @@ if ('undefined' != typeof TlIqCustm && void 0 === TlIqCustm.countryLanguageSelec
 								);
 							},
 							function () {
-								// TlIqCustm.countryLanguageSelector.changeCSS(),
-								TlIqCustm.countryLanguageSelector.setCookie(
-									TlIqCustm.countryLanguageSelector.cookie
+								// TlIqCustm.languageSelectorFooterPhase2.changeCSS(),
+								TlIqCustm.languageSelectorFooterPhase2.setCookie(
+									TlIqCustm.languageSelectorFooterPhase2.cookie
 								);
 							},
 							function () {}
@@ -118,49 +77,50 @@ if ('undefined' != typeof TlIqCustm && void 0 === TlIqCustm.countryLanguageSelec
 
 			ataMain: function () {
 				if (
-					TlIqCustm.countryLanguageSelector.isDesktop() ||
-					TlIqCustm.countryLanguageSelector.isTablet()
+					TlIqCustm.languageSelectorFooterPhase2.isDesktop() ||
+					TlIqCustm.languageSelectorFooterPhase2.isTablet()
 				)
 					try {
 						this.initDomCheck(),
 							TlIqCustm.domCheck(this.dom_check)
-								? TlIqCustm.countryLanguageSelector.moveSection()
-								: TlIqCustm.countryLanguageSelector.setCookie(
-										TlIqCustm.countryLanguageSelector.d_cookie
+								? TlIqCustm.languageSelectorFooterPhase2.moveSection()
+								: TlIqCustm.languageSelectorFooterPhase2.setCookie(
+										TlIqCustm.languageSelectorFooterPhase2.d_cookie
 								  );
 					} catch (e) {
-						TlIqCustm.countryLanguageSelector.setCookie(TlIqCustm.countryLanguageSelector.e_cookie);
+						TlIqCustm.languageSelectorFooterPhase2.setCookie(
+							TlIqCustm.languageSelectorFooterPhase2.e_cookie
+						);
 					}
 			},
 		}),
 			TlIqCustm.poll(
 				function () {
-					return (
-						jQuery('#countrySelector').length &&
-						jQuery('#aa-footer > div.container > div.social').length
-					);
+					return jQuery('#countrySelector').length && jQuery('#aa-footer').length;
 				},
 				function () {
-					TlIqCustm.countryLanguageSelector.ataMain();
+					TlIqCustm.languageSelectorFooterPhase2.ataMain();
 				},
 				function () {
-					(TlIqCustm.countryLanguageSelector.isDesktop() ||
-						TlIqCustm.countryLanguageSelector.isTablet()) &&
-						TlIqCustm.countryLanguageSelector.setCookie(TlIqCustm.countryLanguageSelector.o_cookie);
+					(TlIqCustm.languageSelectorFooterPhase2.isDesktop() ||
+						TlIqCustm.languageSelectorFooterPhase2.isTablet()) &&
+						TlIqCustm.languageSelectorFooterPhase2.setCookie(
+							TlIqCustm.languageSelectorFooterPhase2.o_cookie
+						);
 				}
 			);
 	} catch (e) {
-		(TlIqCustm.countryLanguageSelector.isDesktop() ||
-			TlIqCustm.countryLanguageSelector.isTablet()) &&
+		(TlIqCustm.languageSelectorFooterPhase2.isDesktop() ||
+			TlIqCustm.languageSelectorFooterPhase2.isTablet()) &&
 			(TlIqCustm.ckz.cookieValueExists(
 				'tgck',
-				'BE_OPT0310_HomeRedesignCountryLanguage_Test_Mar2021',
+				'BE_OPT0334_HomeRedesignCountryLanguage_Test_June2021',
 				'|',
 				!1
 			) ||
 				TlIqCustm.ckz.cookieAppend(
 					'tgck',
-					'BE_OPT0310_HomeRedesignCountryLanguage_Test_Mar2021',
+					'BE_OPT0334_HomeRedesignCountryLanguage_Test_June2021',
 					'|'
 				));
 	}
